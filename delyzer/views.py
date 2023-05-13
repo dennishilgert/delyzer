3abc284573a27a929fa8b94e8c7afdefb2685aec
Samuel Matzeit <samuel.matzeit@atos.net>
2023-05-13 22:22:16 +0200

    Changed return value to Station name instead of station id at delay_at_stations

--- delyzer/views.py
+++ delyzer/views.py
@@ -7,7 +7,10 @@ from rest_framework.response import Response
 from django.http import JsonResponse
 from rest_framework import status
 import pandas as pd
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 @api_view(['GET'])
 def departure_list(request):
@@ -26,9 +29,12 @@ def departure_list(request):
     """
 
     if request.method == 'GET':
-        departures = Departure.objects.all()
-        serializer = DepartureSerializer(departures, many=True)
-        return JsonResponse({'departures':serializer.data})
+        try:
+            departures = Departure.objects.all()
+            serializer = DepartureSerializer(departures, many=True)
+            return JsonResponse({'departures':serializer.data})
+        except Exception as e:
+            logger.info(e)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -53,10 +59,13 @@ def departure_detail(request, id: int):
     """
     
     if request.method == 'GET':
-        departure = Departure.objects.get(pk=id)
+        try:
+            departure = Departure.objects.get(pk=id)
 
-        serializer = DepartureSerializer(departure)
-        return JsonResponse(serializer.data)
+            serializer = DepartureSerializer(departure)
+            return JsonResponse(serializer.data)
+        except Exception as e:
+            logger.info(e)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
@@ -78,20 +87,23 @@ def lines(request):
     """
     
     if request.method == 'GET':
-        lines = pd.DataFrame(Departure.objects.values('line_number',
-       'id',
-      'direction',
-      'line_name'))
-        lines = lines.drop_duplicates(subset=['line_number','direction'])
-        lines = lines.sort_values(['line_number','direction'])
-        lines = lines[['line_number','direction']]
-        print(lines)
-        lines = lines.to_dict('records')
-        print(type(lines))
+        try:
+            lines = pd.DataFrame(Departure.objects.values('line_number',
+        'id',
+        'direction',
+        'line_name'))
+            lines = lines.drop_duplicates(subset=['line_number','direction'])
+            lines = lines.sort_values(['line_number','direction'])
+            lines = lines[['line_number','direction']]
+            logger.info(lines)
+            lines = lines.to_dict('records')
+            logger.info(type(lines))
 
 
-        print(lines)
-        return JsonResponse({'lines':lines})
+            logger.info(lines)
+            return JsonResponse({'lines':lines})
+        except Exception as e:
+            logger.info(e)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -112,20 +124,23 @@ def lines_by_delay(request):
     """
     
     if request.method == 'GET':
-        delay = pd.DataFrame(Departure.objects.values('line_number',
-       'id',
-      'direction',
-      'delay',
-      'line_name'))
-        delay = delay.groupby(['line_number','direction'], as_index=False).agg({'delay': 'mean'}).round(2)
-        delay = delay.sort_values('delay', ascending=False)
-        print(delay)
-        delay = delay.to_dict('records')
-        print(type(delay))
+        try:
+            delay = pd.DataFrame(Departure.objects.values('line_number',
+        'id',
+        'direction',
+        'delay',
+        'line_name'))
+            delay = delay.groupby(['line_number','direction'], as_index=False).agg({'delay': 'mean'}).round(2)
+            delay = delay.sort_values('delay', ascending=False)
+            logger.info(delay)
+            delay = delay.to_dict('records')
+            logger.info(type(delay))
 
 
-        print(delay)
-        return JsonResponse({'delays':delay})
+            logger.info(delay)
+            return JsonResponse({'delays':delay})
+        except Exception as e:
+            logger.info(e)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -146,24 +161,27 @@ def delay_at_time(request):
     """
     
     if request.method == 'GET':
-        delay = pd.DataFrame(Departure.objects.values('line_number',
-       'id',
-      'direction',
-      'delay',
-      'line_name',
-      'planned_departure_time'))
-        delay['planned_departure_time'] = delay['planned_departure_time'].apply(lambda x: datetime.datetime.combine(datetime.datetime.today(), x))
-        delay.set_index('planned_departure_time', inplace=True)
-        delay = delay['delay'].resample('30min').mean().round(2).reset_index().ffill()
-        print(delay)
-        delay['delay'] = delay['delay']
-        delay.rename(columns={'planned_departure_time':'timeslot_start'}, inplace=True)
+        try:
+            delay = pd.DataFrame(Departure.objects.values('line_number',
+        'id',
+        'direction',
+        'delay',
+        'line_name',
+        'planned_departure_time'))
+            delay['planned_departure_time'] = delay['planned_departure_time'].apply(lambda x: datetime.datetime.combine(datetime.datetime.today(), x))
+            delay.set_index('planned_departure_time', inplace=True)
+            delay = delay['delay'].resample('30min').mean().round(2).reset_index().ffill()
+            logger.info(delay)
+            delay['delay'] = delay['delay']
+            delay.rename(columns={'planned_departure_time':'timeslot_start'}, inplace=True)
 
-        delay = delay.to_dict('records')
-        
+            delay = delay.to_dict('records')
+            
 
-        print(delay)
-        return JsonResponse({'times':[delay]})
+            logger.info(delay)
+            return JsonResponse({'times':[delay]})
+        except Exception as e:
+            logger.info(e)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -184,18 +202,20 @@ def delay_at_station(request):
     """
     
     if request.method == 'GET':
-        delay = pd.DataFrame(Departure.objects.values('id',
-        'station_id',
-      'delay'))
-        delay = delay.groupby(['station_id'], as_index=False).agg({'delay': 'mean'}).round(2)
-        delay = delay.sort_values('delay', ascending=False)
-        print(delay)
-        delay = delay.to_dict('records')
-        print(type(delay))
+        try:
+            delay = pd.DataFrame(Departure.objects.values('id',
+            'station_id',
+        'delay'))
+            delay = delay.groupby(['station_id'], as_index=False).agg({'delay': 'mean'}).round(2)
+            delay = delay.sort_values('delay', ascending=False)
+            logger.info(delay)
+            delay = delay.to_dict('records')
+            logger.info(type(delay))
 
-
-        print(delay)
-        return JsonResponse({'delays':delay})
+            logger.info(delay)
+            return JsonResponse({'delays':delay})
+        except Exception as e:
+            logger.info(e)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
@@ -216,17 +236,26 @@ def delay_at_station(request):
     """
     
     if request.method == 'GET':
-        delay = pd.DataFrame(Departure.objects.values('id',
-        'station_id',
-      'delay'))
-        delay = delay.groupby(['station_id'], as_index=False).agg({'delay': 'mean'}).round(2)
-        delay = delay.sort_values('delay', ascending=False)
-        print(delay)
-        delay = delay.to_dict('records')
-        print(type(delay))
+        try:
+            delay_df = pd.DataFrame(Departure.objects.values('id',
+            'station_id',
+            'delay'))
+            delay_df = delay_df.groupby(['station_id'], as_index=False).agg({'delay': 'mean'}).round(2)
+            delay_df = delay_df.sort_values('delay', ascending=False)
+
+            stations_info_df = pd.read_csv('vvs_data.csv', sep=',', encoding='utf-8')
+            logger.debug(stations_info_df)
+            delay_df = delay_df.join(stations_info_df.set_index('Nummer'), on='station_id', how="left")
+            delay_df = delay_df[['Name mit Ort', 'delay']]
+            delay_df = delay_df.rename(columns={'Name mit Ort': 'Station'})
+            logger.debug(delay_df)
+            delay_dic = delay_df.to_dict('records')
+            logger.info(type(delay_dic))
 
 
-        print(delay)
-        return JsonResponse({'delays':delay})
+            logger.info(delay_dic)
+            return JsonResponse({'delays':delay_dic})
+        except Exception as e:
+            logger.info(e)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
