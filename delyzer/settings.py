394fcd0e9201cac04564ccaa608b33f211bc58f3
Dennis Hilgert <d.hilgert@laserhub.com>
2023-05-13 17:39:16 +0200

    add logging config for django

--- delyzer/settings.py
+++ delyzer/settings.py
@@ -30,6 +30,47 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+LOGGING = {
+    'version': 1,
+    # The version number of our log
+    'disable_existing_loggers': False,
+    # django uses some of its own loggers for internal operations. In case you want to disable them just replace the False above with true.
+    # A handler for WARNING. It is basically writing the WARNING messages into a file called WARNING.log
+    'handlers': {
+        'file_warning': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'warning.log',
+            'formatter': 'simpleFormatter'
+        },
+        'file_debug': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'debug.log',
+            'formatter': 'simpleFormatter'
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simpleFormatter'
+        },
+    },
+    'formatters': {
+        'simpleFormatter': {
+            'format': '%(levelname)-8s %(asctime)s %(module)-20s %(message)s'
+        }
+    },
+    # A logger for WARNING which has a handler called 'file'. A logger can have multiple handler
+    'loggers': {
+       # notice the blank '', Usually you would put built in loggers like django or root here based on your needs
+        '': {
+            'handlers': ['file_debug', 'file_warning', 'console'], #notice how file variable is called in handler which has been defined above
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    },
+}
+
 INSTALLED_APPS = [
     'rest_framework',
     'delyzer',
