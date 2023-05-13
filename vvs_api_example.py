76e0b094e15ef1a29fb1a7f5f4ebc61bad8e6364
Dennis Hilgert <d.hilgert@laserhub.com>
2023-05-13 17:40:07 +0200

    update example

--- vvs_api_example.py
+++ vvs_api_example.py
@@ -1,6 +1,11 @@
-from vvspy import get_departures
 import pandas as pd
 
 stations_df = pd.read_csv('vvs_data.csv')
 stations_result = stations_df.loc[stations_df['Name mit Ort'].str.contains('Stadtmitte')]
-#print(stations_result)
+
+stations_info_df = pd.read_csv('vvs_haltestellen.csv', sep=';', encoding='cp1252')
+stations_info_result = stations_info_df.loc[stations_info_df['Linien (EFA)'].str.contains('S1')]
+stations_info_result['Nummer'] = stations_info_result['Nummer'].apply(lambda nummer: 5000000 + nummer)
+
+print(stations_info_result)
+
